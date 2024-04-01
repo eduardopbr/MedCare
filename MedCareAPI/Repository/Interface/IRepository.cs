eb2f9e189395bdcbaaddcc8de2dfcfa7e8bc32e2
Eduardo Peroni <eduardo.peroni@hotmail.com>
2024-04-01 13:36:57 -0300

    Adicionar arquivos de projeto.

---
+++ MedCareAPI/Repository/Interface/IRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace MedCareAPI.Repository.Interface
+{
+    public interface IRepository<T>
+    {
+        void Add(T entity);
+        void Delete(T entity);
+        void Update(T entity);
+        IQueryable<T> Get();
+    }
+}
---
+++ MedCareAPI/Repository/Interface/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+
+namespace MedCareAPI.Repository.Interface
+{
+    public interface IUnitOfWork
+    {
+        Task Commit();
+
+        Task<IDbContextTransaction> BeginTransactionAsync();
+    }
+}
